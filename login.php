fa02a1e196048923b50ba01160aa618116de20cc
meemmahmuda <meemmahmuda85@gmail.com>
2025-03-08 23:37:33 +0600

    first commit

---
+++ login.php
@@ -0,0 +1,46 @@
+<?php
+
+$Title = "Login";
+
+include 'includes/header.php';
+
+include 'includes/navbar.php';
+?>
+    
+
+
+
+
+
+    <div class="alert alert-success" role="alert">
+        Login Success !
+    </div>
+
+    <div class="alert alert-danger" role="alert">
+        Invalid Credential !
+    </div>
+
+    <form>
+    <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+    </div>
+    <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Login</button>
+    </form>
+
+
+
+
+
+    <?php
+    include 'includes/footer.php';
+    ?>
